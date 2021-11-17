59d820ec7bc61a25d5b15b16b1f22c01452e0119
Leo Franca <leoarkiteto@gmail.com>
2021-11-17 16:16:07 +0000

    :truck: Move package.json to correct folder

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const request = require('postman-request');
 
 const url =
-  'http://api.weatherstack.com/current?access_key=3e2106e237234f65e56149d50f6db7b6&query=37.8267,-122.4233';
+  'http://api.weatherstack.com/current?access_key=3e2106e237234f65e56149d50f6db7b6&query=37.8267,-122.4233&units=m';
 
-request(url, (error, response, body) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url, json: true }, (error, response, body) => {
+  // console.log(body.current);
+  console.log(
+    `It is current ${body.current.temperature} degrees out. There is a ${body.current.precip}% chance of rain`
+  );
 });
