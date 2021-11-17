0832a1c55dc59393cb118d0799fe2d007a11f4aa
leo Franca <leoarkiteto@gmail.com>
2021-11-17 23:07:03 +0000

    :construction: Add dotenv dependency and create to util functions

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const request = require('postman-request');
+require('dotenv').config();
 
-const url =
-  'http://api.weatherstack.com/current?access_key=3e2106e237234f65e56149d50f6db7b6&query=37.8267,-122.4233&units=m';
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-request({ url, json: true }, (error, response, body) => {
-  console.log(
-    `${body.current.weather_descriptions[0]}. It is current ${body.current.temperature} degrees out. There is a ${body.current.precip}% chance of rain`
-  );
+const WEATHER_KEY = process.env.WEATHER_KEY;
+const MAPBOX_KEY = process.env.MAPBOX_KEY;
+
+geocode('Odivelas', (error, data) => {
+  console.log('Error', error);
+  console.log('Data', data);
 });
 
-// Geocoding
-// Address -> Lat/Long -> Weather
-const geocodingURL =
-  'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibGVvYXJraXRldG8iLCJhIjoiY2t3M3FoOWE4MHJ4cjJvcDZrN3AwdTNwdyJ9.RBjSpdmWZa_cW8exkwb_wg&limit=1';
-
-request({ url: geocodingURL, json: true }, (error, response, body) => {
-  const [longitude, latitude] = body.features[0].center;
-  console.log(longitude, latitude);
+forecast(441545, -75.7088, (error, data) => {
+  console.log('Error', error);
+  console.log('Data', data);
 });
